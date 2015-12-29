968aee51c6de80610b14e42eb75960acd57a16fc
Lars Gardien <larsgardien@live.nl>
2015-12-29 13:20:13 +0100

    Fixed plugin URL-routing

---
+++ MVC_default/TemperatureModule/Areas/Temperature/TemperatureAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC_default.Plugins.TemperatureModule
+{
+    public class TemperatureAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "Temperature"; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Temperature_default",
+                "Temperature/{controller}/{action}/{id}",
+                new { action="Index", id=UrlParameter.Optional }
+                );
+        }
+}
+}
