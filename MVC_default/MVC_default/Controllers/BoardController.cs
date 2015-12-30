a48163628255b983462eb3c40619ef1f65c66b57
Lars Gardien <larsgardien@live.nl>
2015-12-30 21:07:15 +0100

    Re-Upload Merge
    Re-Upload of 7466263361c91ba17b7d9df1a3725cfa2d33653f Merge #1

---
+++ MVC_default/MVC_default/Controllers/BoardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_default.Models;
+using MVC_default.ModuleManager;
+
+namespace MVC_default.Controllers
+{
+    [Authorize]
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult Index()
+        {
+            System.Net.IPAddress localIP = System.Net.IPAddress.Parse("127.0.0.1");
+            var Modules = ModuleManager.ModuleManager.Current.GetModulesDictionary();
+            Modules.Add(new Module { IP = System.Net.IPAddress.Parse("127.0.0.1"), Name = "Temperatuur" }, PluginManager.PluginManager.Current.GetPlugin("TemperatureModule"));
+            Modules.Add(new Module { IP = System.Net.IPAddress.Parse("127.0.0.1"), Name = "Template" }, PluginManager.PluginManager.Current.GetPlugin("TemplatePlugin"));
+            var widgets = Modules.Select(x => new WidgetViewModel { Title = x.Key.Name, WidgetAreaName = x.Value.EntryAreaName, IP = x.Key.IP.ToString(), Width = x.Value.WidgetWidth }).ToList();
+
+            return View(new WidgetListViewModel { Widgets = widgets });        
+        }
+    }
+}
---
+++ MVC_default/MVC_default/Models/BoardViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_default.Models
+{
+    public class WidgetListViewModel
+    {
+        public List<WidgetViewModel> Widgets { get; set; }
+    }
+
+    public class WidgetViewModel
+    {
+        public string Title { get; set; }
+        public string WidgetAreaName { get; set; }
+        public string IP { get; set; }
+
+        public string Width { get; set; }
+    }
+}
