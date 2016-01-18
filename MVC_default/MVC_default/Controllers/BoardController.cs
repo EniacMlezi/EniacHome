34d9e36830519501b2bac0eb7c180fb4f8dfb3d5
Lars Gardien <larsgardien@live.nl>
2016-01-18 20:18:21 +0100

    grid-stack.js dashboard implementation

--- MVC_default/MVC_default/Controllers/BoardController.cs
+++ MVC_default/MVC_default/Controllers/BoardController.cs
@@ -15,7 +15,12 @@ namespace MVC_default.Controllers
         public ActionResult Index()
         {
             var Modules = ModuleManager.ModuleManager.Current.GetModulesDictionary();
-            var widgets = Modules.Select(x => new WidgetViewModel { Title = x.Key.Name, WidgetAreaName = x.Value.EntryAreaName, IP = x.Key.IP.ToString(), Width = x.Value.WidgetWidth }).ToList();
+            //var widgets = Modules.Select(x => new WidgetViewModel { Title = x.Key.Name, WidgetAreaName = x.Value.EntryAreaName, IP = x.Key.IP.ToString(),  }).ToList();
+
+            List<WidgetViewModel> widgets = new List<WidgetViewModel>();
+            widgets.Add(new WidgetViewModel { Title = "testTemp", WidgetAreaName="Temperature", IP= "halloHACK", Size="2" });
+            widgets.Add(new WidgetViewModel { Title = "testTemp", WidgetAreaName = "Temperature", IP = "halloHACK", Size="4" });
+            widgets.Add(new WidgetViewModel { Title = "testTemp", WidgetAreaName = "Template", IP = "halloHACK", Size="3" });
 
             return View(new WidgetListViewModel { Widgets = widgets });        
         }
