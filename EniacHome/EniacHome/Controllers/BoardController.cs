2b2c95d46d2ca41c581f015133e15445e9cfb99f
Lars Gardien <larsgardien@live.nl>
2016-01-30 11:52:51 +0100

    Dashboard cookie

--- EniacHome/EniacHome/Controllers/BoardController.cs
+++ EniacHome/EniacHome/Controllers/BoardController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using EniacHome.Models;
 using EniacHome.ModuleManager;
+using System.Web.Script.Serialization;
 
 namespace EniacHome.Controllers
 {
@@ -15,6 +16,7 @@ namespace EniacHome.Controllers
         public ActionResult Index()
         {   
             var Modules = ModuleManager.ModuleManager.Current.GetModules();
+
             var widgets = Modules.Select(x => new WidgetViewModel
             {
                 Title = x.Name,
@@ -22,11 +24,30 @@ namespace EniacHome.Controllers
                 IP = x.Socket.RemoteEndPoint.ToString(),
                 WidgetWidth = x.Plugin.WidgetWidth,
                 WidgetHeight = x.Plugin.WidgetHeight,
-                //PositionX = 
-                //PositionY =
+                PositionX = (Request.Cookies["Dashboard"] == null) ? null : Request.Cookies["Dashboard"][x.Name].Split(',')[0],
+                PositionY = (Request.Cookies["Dashboard"] == null) ? null : Request.Cookies["Dashboard"][x.Name].Split(',')[1]
             }).ToList();
 
             return View(new WidgetListViewModel { Widgets = widgets });
+        }
+
+        private struct widget
+        {
+            public string Name { get; set; }
+            public string x { get; set; }
+            public string y { get; set; }
+        }
+        public void UpdateCookie(string[] widgets)
+        {
+            HttpCookie DashboardCookie = new HttpCookie("Dashboard");
+            foreach (string widget in widgets)
+            {
+                widget objWidget = new JavaScriptSerializer().Deserialize<widget>(widget);
+                DashboardCookie[objWidget.Name] = objWidget.x + "," + objWidget.y;
+            }
+            DashboardCookie.Expires = DateTime.Now.AddYears(10);
+            Response.Cookies.Add(DashboardCookie);  
+            return;
         }
     }
 }
