97e23e9a90bf614c75c2075f58d8dd7b8a9eb026
Lars Gardien <larsgardien@live.nl>
2016-01-30 11:55:45 +0100

    ModuleFinder missing files

---
+++ EniacHome/EniacHome/Areas/Admin/Controllers/ModuleFinderController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EniacHome.Areas.Admin.Models;
+using ModuleFinderInterface;
+
+namespace EniacHome.Areas.Admin.Controllers
+{
+
+    public class ModuleFinderController : Controller
+    {
+        [Authorize(Roles = "Administrator, Moderator")]
+        // GET: Admin/ModuleFinder
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            IEnumerable<IModuleFinder> moduleFinders = ModuleFinderManager.ModuleFinderManager.Current.GetModuleFinders();
+            IEnumerable<ModuleFinderDetailsViewModel> model = moduleFinders.Select(x => new ModuleFinderDetailsViewModel {
+                Name = x.Name,
+                Author = x.Author,
+                Version = x.Version.ToString(),
+                IsListening = x.IsListening
+            });
+            return View(model);
+        }
+
+        public ActionResult Details(string Name)
+        {
+            IModuleFinder moduleFinder = ModuleFinderManager.ModuleFinderManager.Current.GetModuleFinder(Name);
+            ModuleFinderDetailsViewModel model = new ModuleFinderDetailsViewModel
+            {
+                Name = moduleFinder.Name,
+                Author = moduleFinder.Author,
+                Version = moduleFinder.Version.ToString(),
+                IsListening = moduleFinder.IsListening
+            };
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string Name)
+        {
+            System.IO.File.Delete(Server.MapPath("~/plugins/" + Name + ".dll"));
+            ModuleFinderManager.ModuleFinderManager.Current.Reload();
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        [ValidateHeaderAntiForgeryToken]
+        public ActionResult Add()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                HttpPostedFileBase file = Request.Files[i];
+                file.SaveAs(Server.MapPath("~/plugins/") + file.FileName);
+            }
+            ModuleFinderManager.ModuleFinderManager.Current.Reload();
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void ToggleListening(string Name)
+        {
+            IModuleFinder ModuleFinder = ModuleFinderManager.ModuleFinderManager.Current.GetModuleFinder(Name);
+            if (!ModuleFinder.IsListening)           
+                ModuleFinder.StartListening();           
+            else           
+                ModuleFinder.StopListening();
+        }
+    }
+}
---
+++ EniacHome/EniacHome/Areas/Admin/Models/ModuleFinderViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EniacHome.Areas.Admin.Models
+{
+    public class ModuleFinderListViewModel
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class ModuleFinderDetailsViewModel
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Version { get; set; }
+
+        public bool IsListening { get; set; }
+    }
+}
